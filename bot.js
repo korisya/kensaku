6ede5d4f177c067f0268a2675d9666353b324d99
t3alizes <josephhsieh13@gmail.com>
2018-11-18 17:49:49 -0800

    read cookies and token from secret.txt, output player session start time

--- bot.js
+++ bot.js
@@ -2,11 +2,18 @@ var Discord = require('discord.js');
 var rp = require('request-promise');
 var tough = require('tough-cookie');
 const cheerio = require('cheerio');
+var fs = require('fs');
 
-var msMinute = 60*1000;
-var msHour = 60*60*1000;
-var date = new Date();
-date.setHours(date.getHours() - 16);
+// -----------Remove if you want to paste the cookies and tokens into the code itself.-----------
+console.log('Finding cookies and cream:');
+var secrets = fs.readFileSync('secret.txt', {encoding: 'utf8'}).trim().split(' ');
+console.log('cream: ' + secrets[0]);
+for (var i = 1; i < secrets.length; i++)
+	console.log('cookie ' + i + ': ' + secrets[i]);
+// ----------------------------------------------------------------------------------------------
+
+const msMinute = 60*1000;
+const msHour = 60*60*1000;
 
 // Constructor for locations
 function Location (val, name) {
@@ -29,18 +36,26 @@ function Location (val, name) {
 
 	this.playerNames = [];
 	this.playerDates = [];
+	this.sandList = [];
+	this.sandTime = [];
 
 	this.getOutput = function () {
 		var currentTime = new Date();
 		var output = '';
 		for (var i = 0; i < this.playerNames.length; i++) {
-			var hr = Math.floor((currentTime - this.playerDates[i]) / msHour);
-			var min = Math.floor(((currentTime - this.playerDates[i]) % msHour) / msMinute);
-			if (hr <= 15) {
-				output += this.playerNames[i]
-				for (var j = this.playerNames[i].length; j < 8; j++)
-					output += ' ';
-				output += '              (' + hr + 'h ' + min +  'm)' + '\n';
+			for (var j = 0; j < this.sandList.length; j++) {
+				if (this.sandList[j] === this.playerNames[i]) {
+					var hr = Math.floor((currentTime - this.playerDates[i]) / msHour);
+					var min = Math.floor(((currentTime - this.playerDates[i]) % msHour) / msMinute);
+
+					output += this.playerNames[i]
+					for (var k = this.playerNames[i].length; k < 8; k++)
+						output += ' ';
+
+					output += '   ' + this.sandTime[j].toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+					output += '   Seen ' + hr + 'h ' + min +  'm ago\n';
+					break;
+				}
 			}
 		}
 		return output || ' ';
@@ -51,13 +66,14 @@ function Location (val, name) {
 async function getInitialData(loc) {
 	await rp(loc.options)
 		.then(($) => {
+			var date = new Date();
 			// Parses data
-			for (var i = 1; i < $('.dancer_name').get().length - 10; i++) { // i = 1 because first value isn't a player name
+			for (var i = 1; i < $('.dancer_name').get().length; i++) { // i = 1 because first value isn't a player name
 				loc.playerNames[i-1] = $('.dancer_name').eq(i).text();
 				loc.playerDates[i-1] = date;
-				console.log(loc.name + ': Player ' + i + ' received');
+				console.log(loc.name + ': Player ' + i + ' received - ' + loc.playerNames[i-1]);
 			}
-			loc.firstRetrieval = false;
+
 			setTimeout(function() {
 				retrieveData(loc) }, 60000);
 		})
@@ -68,6 +84,12 @@ async function getInitialData(loc) {
 
 // Retrieves new data every minute
 async function retrieveData(loc) {
+	var currentTime = new Date();
+	if (currentTime.getHours() == 4 && loc.sandList.length != 0) {
+		loc.sandList = [];
+		loc.sandTime = [];
+	}
+
 	console.log(loc.name + ': Retrieving data...');
 	await rp(loc.options)
 		.then(($) => {
@@ -75,16 +97,36 @@ async function retrieveData(loc) {
 			// if the first two players are different
 			if (loc.playerNames[0] !== $('.dancer_name').eq(1).text() && loc.playerNames[0] !== $('.dancer_name').eq(2).text()) {
 				var popIndex = 2; // popIndex indicates the number of times to call the pop() method
+
 				// move player info 2 places down the arrays
 				for (var i = loc.playerNames.length; i > 0; i--) {
 					loc.playerNames[i+1] = loc.playerNames[i-1];
 					loc.playerDates[i+1] = loc.playerDates[i-1];
 				}
+
 				// add the new info to the beginning of the arrays
 				loc.playerNames[0] = $('.dancer_name').eq(1).text();
 				loc.playerNames[1] = $('.dancer_name').eq(2).text();
-				loc.playerDates[0] = new Date();
-				loc.playerDates[1] = new Date();
+				loc.playerDates[0] = currentTime;
+				loc.playerDates[1] = currentTime;
+
+				// checks if this is the start of a player's session. if true, initialize their start time
+				var newSession0 = true, newSession1 = true;
+				for (var j = 0; j < loc.sandList.length; j++) {
+					if (loc.playerNames[0] === loc.sandList[j])
+						newSession0 = false;
+					else if (loc.playerNames[1] === loc.sandList[j])
+						newSession1 = false;
+				}
+				if (newSession0) {
+					loc.sandList[loc.sandList.length] = loc.playerNames[0];
+					loc.sandTime[loc.sandTime.length] = currentTime;
+				}
+				if (newSession1) {
+					loc.sandList[loc.sandList.length] = loc.playerNames[1];
+					loc.sandTime[loc.sandTime.length] = currentTime;
+				}
+
 				// removes duplicates
 				for (var i = 2; i < loc.playerNames.length; i++) {
 					if (loc.playerNames[0] === loc.playerNames[i]) {
@@ -98,6 +140,7 @@ async function retrieveData(loc) {
 						popIndex -= 1;
 					}
 				}
+
 				// removes players off the end of the list
 				for (var i = 0; i < popIndex; i++) {
 					loc.playerNames.pop();
@@ -108,14 +151,29 @@ async function retrieveData(loc) {
 			// else, if the first player is different
 			else if (loc.playerNames[0] !== $('.dancer_name').eq(1).text()) {
 				var popIndex = 1;
+
 				// moves player info 1 place down each array
 				for (var i = loc.playerNames.length; i > 0; i--) {
 					loc.playerNames[i] = loc.playerNames[i-1];
 					loc.playerDates[i] = loc.playerDates[i-1];
 				}
+
 				// add the new info to the beginning of the arrays
 				loc.playerNames[0] = $('.dancer_name').eq(1).text();
-				loc.playerDates[0] = new Date();
+				loc.playerDates[0] = currentTime;
+
+				// checks whether this is the start of a player's session. if true, appends them to sandList
+				var newSession0 = true;
+				for (var j = 0; j < loc.sandList.length; j++) {
+					if (loc.playerNames[0] === loc.sandList[j]) {
+						newSession0 = false;
+						break;
+					}
+				}
+				if (newSession0) {
+					loc.sandList[loc.sandList.length] = loc.playerNames[0];
+					loc.sandTime[loc.sandTime.length] = currentTime;
+				}
 
 				// removes duplicates
 				for (var i = 1; i < loc.playerNames.length; i++) {
@@ -125,6 +183,7 @@ async function retrieveData(loc) {
 						popIndex -= 1; // because one value was removed, the list needs to be shortened one less value
 					}
 				}
+
 				// removes players off the end of the list
 				for (var i = 0; i < popIndex; i++) {
 					loc.playerNames.pop();
@@ -162,11 +221,11 @@ function getRinonMesssage() {
 
 
 // Initalize locations
-var milpitas = new Location("", "Milpitas"); // PASTE COOKIE HERE
-var sanJoseJ = new Location("", "San Jose J-Cab"); // PASTE COOKIE HERE
-var sanJoseK = new Location("", "San Jose K-Cab"); // PASTE COOKIE HERE
-var dalyCity = new Location("", "Daly City"); // PASTE COOKIE HERE
-var concord = new Location("", "Concord"); // PASTE COOKIE HERE
+var milpitas = new Location(secrets[1], "Milpitas"); // PASTE COOKIE HERE iF NOT USING secret.txt
+var sanJoseJ = new Location(secrets[2], "San Jose J-Cab"); // PASTE COOKIE HERE iF NOT USING secret.txt
+var sanJoseK = new Location(secrets[3], "San Jose K-Cab"); // PASTE COOKIE HERE iF NOT USING secret.txt
+var dalyCity = new Location(secrets[4], "Daly City"); // PASTE COOKIE HERE iF NOT USING secret.txt
+var concord = new Location(secrets[5], "Concord"); // PASTE COOKIE HERE iF NOT USING secret.txt
 
 // Initialize Discord Bot
 var bot = new Discord.Client();
@@ -175,9 +234,9 @@ bot.on('ready', () => {
 	console.log('Connected');
 	console.log(`Logged in as ${bot.user.tag}!`);
 	console.log(bot.user.username + ' - (' + bot.user.id + ')');
+	getInitialData(milpitas);
 	getInitialData(sanJoseJ);
 	getInitialData(sanJoseK);
-	getInitialData(milpitas);
 	getInitialData(dalyCity);
 	getInitialData(concord);
 });
@@ -188,7 +247,7 @@ bot.on('message', message => {
 		var cmd = args[0];
 		var subcmd = args[1];
 		var channel = message.channel.name;
-		if(cmd === 'whose') {
+		if (cmd === 'whose') {
 			switch (channel) {
 				case 'dnb-milpitas':
 					message.channel.send({embed:{
@@ -241,4 +300,4 @@ bot.on('message', message => {
 	}
 });
 
-bot.login(''); // PASTE TOKEN HERE
+bot.login(secrets[0]); // PASTE TOKEN HERE iF NOT USING secret.txt
